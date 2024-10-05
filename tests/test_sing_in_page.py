8bf6c49337b544ffaf9cc38ba8b84c1345eb30d6
PC <skachat30913@gmail.com>
2024-10-06 00:59:14 +0400

    !

--- tests/test_sing_in_page.py
+++ tests/test_sing_in_page.py
@@ -13,6 +13,6 @@ def test_sing_in_page(login_page):
 def test_sing_in_page_incorrect_password(login_page):
     login_page.open()
     login_page.sing_in_click()
-    login_page.fill_login_form('123abc123abc@gmai.com', 'm96')
+    login_page.fill_login_form('123abc1235abc@gmai.com', 'm96')
     login_page.check_error_text(
         'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
